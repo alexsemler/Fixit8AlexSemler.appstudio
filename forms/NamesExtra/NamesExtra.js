c417abbae21b3eb807a0279db8fec1e9b491496d
alexsemler <alex.semler03@gmail.com>
2019-09-25 14:22:25 -0500

    Version as of 2:22

---
+++ forms/NamesExtra/NamesExtra.js
@@ -0,0 +1,5 @@
+let array = ["Bob", "Janet", "Tom", "Bob", "Randy", "Elizabeth", "Kim", "Nancy"]
+
+for (i=array.length-1; i >= 0; i--) {
+  console.log(array[i])
+  }
---
+++ forms/WhileExtra/WhileExtra.js
@@ -0,0 +1,11 @@
+/*
+let first = prompt()
+let last = prompt()
+let i = 0
+
+while (i < first.length) {
+  console.log(`letter ${i+1} is ${first.slice(i,i+1)}`)
+  i++
+  }
+  
+*/
